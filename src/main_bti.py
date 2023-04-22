5dc966268405f7dd042db64c42ea62fcd7467490
Reggie CUshing <r.cushing@esciencecenter.nl>
2023-04-22 13:18:06 +0200

    add file check to bti parser

--- src/main_bti.py
+++ src/main_bti.py
@@ -9,6 +9,8 @@ def main():
     parser = argparse.ArgumentParser(description='Parse bti country report.')
     parser.add_argument('--overwrite', action='store_true',
                         help='overwrite output file.')
+    parser.add_argument('--check_files', action='store_true',
+                        help='check that files in data folder exist in output folder as csv.')
     parser.add_argument('--datafolder', nargs='?', default='./',
                         help='output folder for json files.')
     parser.add_argument('--outputfolder', nargs='?', default='./',
@@ -32,10 +34,23 @@ def main():
     #        config = json.loads(file.read())
 
     all_files = glob.glob(os.path.join(args.datafolder, '*'))
+    error_cnt = 0
     for file in all_files:
-        (done, file, time, ver) = convert_to_csv(file, args.outputfolder, overwrite=args.overwrite)
-        if done:
-            print(f"converted {file} with bti parser version {ver} at {time}.")
+        if args.check_files:
+            file_name = os.path.splitext(os.path.basename(file))[0]
+            output_file = os.path.join(args.outputfolder, file_name + ".csv")
+            if os.path.exists(output_file) is False:
+                error_cnt += 1
+                print(f"[error] file {output_file} does not exist")
+        else:
+            (done, file, time, ver) = convert_to_csv(file, args.outputfolder, overwrite=args.overwrite)
+            if done:
+                print(f"converted {file} with bti parser version {ver} at {time}.")
+            else:
+                print(f"[error] converting {file} with bti parser version {ver} at {time}.")
+                error_cnt += 1
+    if error_cnt == 0:
+        print("all done.")
 
 
 main()
